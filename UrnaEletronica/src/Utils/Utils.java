202d909ce7327e3dd21d07fbdf1fc6d8f867a653
Isaías Faria - Notebook Pessoal <isaias@isaiasfaria.com.br>
2015-09-30 09:54:15 -0300

    Separação métodos loadCargos, loadPartidos e loadCandidatos

--- UrnaEletronica/src/Utils/Utils.java
+++ UrnaEletronica/src/Utils/Utils.java
@@ -3,6 +3,11 @@
   - Podemos pensar em leitura de arquivos e afins.
  */
 package Utils;
+import Eleicao.Candidatos;
+import Eleicao.Eleicao;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import urnaeletronica.*;
 
 
@@ -18,7 +23,43 @@ public class Utils {
         else 
             return A;
     }
+
+//carrega todos os cargos
+    public static void loadCargos(Eleicao e){
+        try (BufferedReader br = new BufferedReader(new FileReader("cargos.txt"))){
+            String sCurrentLine;
+            while ((sCurrentLine = br.readLine()) != null) {
+                   String[] s = sCurrentLine.split(";"); // Pega o ID em s[0] e o nome em s[1]
+                   e.addCargo(Integer.parseInt(s[0]), s[1]);
+            }
+        } catch (IOException error) {
+                System.out.println("Erro ao importar os cargos. Abortando operação agora!");
+        }   
+    }
     
-    
-   
+//Carrega todos os partidos
+    public static void loadPartidos(Eleicao e){
+        try (BufferedReader br = new BufferedReader(new FileReader("partidos.txt"))){
+            String sCurrentLine;
+            while ((sCurrentLine = br.readLine()) != null) {
+                   String[] s = sCurrentLine.split(";"); // Pega o ID em s[0] e o nome em s[1]
+                   e.addPartido(Integer.parseInt(s[0]), s[1]);
+            }
+        } catch (IOException error) {
+                System.out.println("Erro ao importar os Partidos. Abortando operação agora!");
+        }
+    }
+
+//Carrega todos os candidatos
+    public static void loadCandidatos(Eleicao e){
+        try (BufferedReader br = new BufferedReader(new FileReader("candidatos.txt"))){
+            String sCurrentLine;
+            while ((sCurrentLine = br.readLine()) != null) {
+                   String[] s = sCurrentLine.split(";"); // Pega o ID em s[0] e o nome em s[1]
+                   e.addCandidato(Integer.parseInt(s[0]), s[1],e.getCargoByID(Integer.parseInt(s[2])), e.getPartidoByID(Integer.parseInt(s[2])));
+            }
+        } catch (IOException error) {
+                System.out.println("Erro ao importar os Candidatos. Abortando operação agora!");
+        }
+    }
 }
--- UrnaEletronica/src/urnaeletronica/UrnaEletronica.java
+++ UrnaEletronica/src/urnaeletronica/UrnaEletronica.java
@@ -3,6 +3,7 @@
    -Nessa classe vamos instanciar os objetos e chamar os métodos das outras classes
  */
 package urnaeletronica;
+import Eleicao.*;
 import Utils.*;
 import java.io.BufferedReader;
 import java.io.FileReader;
@@ -19,43 +20,10 @@ public class UrnaEletronica {
     public static void main(String[] args) {
        Eleicao e = new Eleicao();
         
-    //Lembrar de adicionar os métodos de importar dentro de ELEIÇÃO !?? oU DENTRO DE UTILS?    
-        //carrega todos os cargos
-        try (BufferedReader br = new BufferedReader(new FileReader("cargos.txt"))){
-            String sCurrentLine;
-            while ((sCurrentLine = br.readLine()) != null) {
-                   String[] s = sCurrentLine.split(";"); // Pega o ID em s[0] e o nome em s[1]
-                   e.addCargo(Integer.parseInt(s[0]), s[1]);
-            }
-        } catch (IOException error) {
-                System.out.println("Erro ao importar os cargos. Abortando operação agora!");
-        } 
-        
-        
-        //carrega todos os Partidos
-        try (BufferedReader br = new BufferedReader(new FileReader("partidos.txt"))){
-            String sCurrentLine;
-            while ((sCurrentLine = br.readLine()) != null) {
-                   String[] s = sCurrentLine.split(";"); // Pega o ID em s[0] e o nome em s[1]
-                   e.addPartido(Integer.parseInt(s[0]), s[1]);
-            }
-        } catch (IOException error) {
-                System.out.println("Erro ao importar os Partidos. Abortando operação agora!");
-        } 
-        
-        
-        //carrega todos os Candidatos
-        try (BufferedReader br = new BufferedReader(new FileReader("candidatos.txt"))){
-            String sCurrentLine;
-            while ((sCurrentLine = br.readLine()) != null) {
-                   String[] s = sCurrentLine.split(";"); // Pega o ID em s[0] e o nome em s[1]
-                   e.addCandidato(Integer.parseInt(s[0]), s[1],e.getCargoByID(Integer.parseInt(s[2])), e.getPartidoByID(Integer.parseInt(s[2])));
-            }
-        } catch (IOException error) {
-                System.out.println("Erro ao importar os Candidatos. Abortando operação agora!");
-        } 
-        
-        
+    // Carrega ambiente (Cargos, Partidos, Candidatos)    
+        Utils.loadCargos(e);
+        Utils.loadPartidos(e);
+        Utils.loadCandidatos(e);
         
         e.listaCandidatos();
        
